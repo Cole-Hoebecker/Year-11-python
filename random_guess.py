698faa762b9f7f5f62f16bb69ac75525b88bb9e2
Jasper <jasper.tan@education.nsw.gov.au>
2025-09-22 23:05:52 +0000

    nigeria

--- random_guess.py
+++ random_guess.py
@@ -1,17 +1,22 @@
 import random
 random_num = random.randint(1,1000)
 guesses = 0
+max_guess = 1000
+min_guess = 0
 while guesses < 10:
-    guess = int(input("Guess a number from 1 - 1000 inclusive. "))
-    if guess == random_num:
-        print(f"Your guess, {guess}, is correct! You did it in {guesses} guesses!")
-        break
-    elif guess < random_num:
-        print("The number is higher than you guessed.")
+    if max_guess < guess < min_guess:
+        guess = int(input("Guess a number from 1 - 1000 inclusive. "))
+        if guess == random_num:
+            print(f"Your guess, {guess}, is correct! You did it in {guesses} guesses!")
+            break
+        elif guess < random_num:
+            print("The number is higher than you guessed.")
+        else:
+            print("The number is lower than you guessed.")
+        guesses += 1
+        print(f"Guesses done: {guesses}")
     else:
-        print("The number is lower than you guessed.")
-    guesses += 1
-    print(f"Guesses done: {guesses}")
+        print("Number not between 1 - 1000!")
 if guesses == 10:
     print("Max number of guesses done. YOU LOSE!")
     print(f"The number was {random_num}")
